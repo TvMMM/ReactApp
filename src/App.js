d4e54723ab0d5ed61865250d5e98ca4fe0c56f51
Jackparo <zingme321@gmail.com>
2020-06-26 21:38:00 +0700

    render product

--- src/App.js
+++ src/App.js
@@ -1,134 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import data from './data'
 
 function App() {
-
-  const openMenu = () => {
-    document.querySelector(".sidebar").classList.add("open");
-  }
-  const closeMenu = () =>{
-    document.querySelector(".sidebar").classList.remove("open");
-  }
-  return (
-    <div className="grid-container">
-            <header className="header">
-                <div className="brand">
-                    <button onClick={openMenu}>
-                        &#9776;
-                    </button>
-                    <a href="index.html">ETravel</a>
-                </div>
-                <div className="header-links">
-                    <a href="cart.html">Cart</a>
-                    <a href="signin.html">Sign-in</a>
-                </div>
-            </header>
-            <aside className="sidebar">
-                <h3>Continent</h3>
-                <button className="sidebar-close-button" onClick={closeMenu}>x</button>
-                <ul>
-                    <li>
-                        <a href="index.html">Africa</a>
-                    </li>
-                    <li>
-                        <a href="index.html">Europe</a>
-                    </li>
-                    <li>
-                        <a href="index.html">Asia</a>
-                    </li>
-                    <li>
-                        <a href="index.html">North America</a>
-                    </li>
-                    <li>
-                        <a href="index.html">South America</a>
-                    </li>
-                </ul>
-            </aside>
-            <main className="main">
-                <div className="content">
-                    <ul className="products">
+    
+    const openMenu = () => {
+        document.querySelector(".sidebar").classList.add("open");
+    }
+    const closeMenu = () =>{
+        document.querySelector(".sidebar").classList.remove("open");
+    }
+    return (
+        <div className="grid-container">
+                <header className="header">
+                    <div className="brand">
+                        <button onClick={openMenu}>
+                            &#9776;
+                        </button>
+                        <a href="index.html">ETravel</a>
+                    </div>
+                    <div className="header-links">
+                        <a href="cart.html">Cart</a>
+                        <a href="signin.html">Sign-in</a>
+                    </div>
+                </header>
+                <aside className="sidebar">
+                    <h3>Continent</h3>
+                    <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+                    <ul>
                         <li>
-                            <div className="product">
-                                <img className="product-image" src="https://picsum.photos/200/300" alt="product"/>
-                                <div className="product-name">
-                                    <a href="product.html">Product product-name</a>
-                                </div>
-                                <div className="product-continent">Continent</div>
-                                <div className="product-price">$</div>
-                                <div className="product-rating">4.5 Star(10 Review)</div>
-                            </div>
+                            <a href="index.html">Africa</a>
                         </li>
-
                         <li>
-                            <div className="product">
-                                <img className="product-image" src="https://picsum.photos/200/300" alt="product"/>
-                                <div className="product-name">
-                                    <a href="product.html">Product product-name</a>
-                                </div>
-                                <div className="product-continent">Continent</div>
-                                <div className="product-price">$</div>
-                                <div className="product-rating">4.5 Star(10 Review)</div>
-                            </div>
+                            <a href="index.html">Europe</a>
                         </li>
-
                         <li>
-                            <div className="product">
-                                <img className="product-image" src="https://picsum.photos/200/300" alt="product"/>
-                                <div className="product-name">
-                                    <a href="product.html">Product product-name</a>
-                                </div>
-                                <div className="product-continent">Continent</div>
-                                <div className="product-price">$</div>
-                                <div className="product-rating">4.5 Star(10 Review)</div>
-                            </div>
+                            <a href="index.html">Asia</a>
                         </li>
-
                         <li>
-                            <div className="product">
-                                <img className="product-image" src="https://picsum.photos/200/300" alt="product"/>
-                                <div className="product-name">
-                                    <a href="product.html">Product product-name</a>
-                                </div>
-                                <div className="product-continent">Continent</div>
-                                <div className="product-price">$</div>
-                                <div className="product-rating">4.5 Star(10 Review)</div>
-                            </div>
+                            <a href="index.html">North America</a>
                         </li>
-
                         <li>
-                            <div className="product">
-                                <img className="product-image" src="https://picsum.photos/200/300" alt="product"/>
-                                <div className="product-name">
-                                    <a href="product.html">Product product-name</a>
-                                </div>
-                                <div className="product-continent">Continent</div>
-                                <div className="product-price">$</div>
-                                <div className="product-rating">4.5 Star(10 Review)</div>
-                            </div>
+                            <a href="index.html">South America</a>
                         </li>
-
-                        <li>
-                            <div className="product">
-                                <img className="product-image" src="https://picsum.photos/200/300" alt="product"/>
-                                <div className="product-name">
-                                    <a href="product.html">Product product-name</a>
-                                </div>
-                                <div className="product-continent">Continent</div>
-                                <div className="product-price">$</div>
-                                <div className="product-rating">4.5 Star(10 Review)</div>
-                            </div>
-                        </li>
-                        
                     </ul>
-                </div>
-                
-            </main>
+                </aside>
+                <main className="main">
+                    <div className="content">
+                        <ul className="products">
+                            {
+                                data.products.map(product => 
+                                <li>
+                                    <div className="product">
+                                        <img className="product-image" src={product.image} alt="product"/>
+                                        <div className="product-name">
+                                            <a href="product.html">{product.name}</a>
+                                        </div>
+                                        <div className="product-continent">{product.continent}</div>
+                                        <div className="product-price">${product.price}</div>
+                                        <div className="product-rating">{product.rating} Stars</div>
+                                    </div>
+                                </li>
+                            )
+                            }
+                        </ul>
+                    </div>
+                </main>
             <footer className="footer">
                 All right reserved.
             </footer>
         </div>
-  );
+    );
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,24 @@
+export default  {
+    products: [{
+        name: 'USA',
+        image: 'https://loremflickr.com/320/240/usa',
+        price: 1100,
+        continent:'North America',
+        rating: 5
+    },
+    {
+        name: 'China',
+        image: 'https://loremflickr.com/320/240/china',
+        price: 900,
+        continent:'Asian',
+        rating: 4
+    },
+    {
+        name: 'Italy',
+        image: 'https://loremflickr.com/320/240/italy',
+        price: 990,
+        continent:'Europe',
+        rating: 4.5
+    }
+    ]
+}
