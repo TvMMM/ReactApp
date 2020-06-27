577c1c6fbcd56d6d8e53455854ac28a81a2e8a04
TvM <minhtvgcs18492@fpt.edu.vn>
2020-06-27 11:12:13 +0700

    Routing & Product Detail page

--- src/App.js
+++ src/App.js
@@ -1,75 +1,64 @@
 import React from 'react';
 import './App.css';
-import data from './data'
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
 
 function App() {
-    
     const openMenu = () => {
         document.querySelector(".sidebar").classList.add("open");
     }
     const closeMenu = () =>{
         document.querySelector(".sidebar").classList.remove("open");
     }
+    
     return (
+    <BrowserRouter>
         <div className="grid-container">
-                <header className="header">
-                    <div className="brand">
-                        <button onClick={openMenu}>
-                            &#9776;
-                        </button>
-                        <a href="index.html">ETravel</a>
-                    </div>
-                    <div className="header-links">
-                        <a href="cart.html">Cart</a>
-                        <a href="signin.html">Sign-in</a>
-                    </div>
-                </header>
-                <aside className="sidebar">
-                    <h3>Continent</h3>
-                    <button className="sidebar-close-button" onClick={closeMenu}>x</button>
-                    <ul>
-                        <li>
-                            <a href="index.html">Africa</a>
-                        </li>
-                        <li>
-                            <a href="index.html">Europe</a>
-                        </li>
-                        <li>
-                            <a href="index.html">Asia</a>
-                        </li>
-                        <li>
-                            <a href="index.html">North America</a>
-                        </li>
-                        <li>
-                            <a href="index.html">South America</a>
-                        </li>
-                    </ul>
-                </aside>
-                <main className="main">
-                    <div className="content">
-                        <ul className="products">
-                            {
-                                data.products.map(product => 
-                                <li>
-                                    <div className="product">
-                                        <img className="product-image" src={product.image} alt="product"/>
-                                        <div className="product-name">
-                                            <a href="product.html">{product.name}</a>
-                                        </div>
-                                        <div className="product-continent">{product.continent}</div>
-                                        <div className="product-price">${product.price}</div>
-                                        <div className="product-rating">{product.rating} Stars</div>
-                                    </div>
-                                </li>
-                            )
-                            }
-                        </ul>
-                    </div>
-                </main>
+            <header className="header">
+                <div className="brand">
+                    <button onClick={openMenu}>
+                        &#9776;
+                    </button>
+                    <Link to="/">ETravel</Link>
+                </div>
+                <div className="header-links">
+                    <a href="cart.html">Cart</a>
+                    <a href="signin.html">Sign-in</a>
+                </div>
+            </header>
+            <aside className="sidebar">
+                <h3>Continent</h3>
+                <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+                <ul>
+                    <li>
+                        <a href="index.html">Africa</a>
+                    </li>
+                    <li>
+                        <a href="index.html">Europe</a>
+                    </li>
+                    <li>
+                        <a href="index.html">Asia</a>
+                    </li>
+                    <li>
+                        <a href="index.html">North America</a>
+                    </li>
+                    <li>
+                        <a href="index.html">South America</a>
+                    </li>
+                </ul>
+            </aside>
+            <main className="main">
+                <div className="content">
+                    <Route path="/product/:id" exact component={ProductScreen}/>
+                    <Route path="/" exact component={HomeScreen}/>
+                </div>
+            </main>
             <footer className="footer">
                 All right reserved.
             </footer>
         </div>
+    </BrowserRouter>
     );
 }
 
--- src/data.js
+++ src/data.js
@@ -1,22 +1,26 @@
 export default  {
     products: [{
+        _id:'1',
         name: 'USA',
         image: 'https://loremflickr.com/320/240/usa',
-        price: 1100,
+        price: '1100',
         continent:'North America',
-        rating: 5
+        rating: 5,
+        description: 'abc'
     },
     {
+        _id:'2',
         name: 'China',
         image: 'https://loremflickr.com/320/240/china',
-        price: 900,
+        price: '900',
         continent:'Asian',
         rating: 4
     },
     {
+        _id:'3',
         name: 'Italy',
         image: 'https://loremflickr.com/320/240/italy',
-        price: 990,
+        price: '990',
         continent:'Europe',
         rating: 4.5
     }
